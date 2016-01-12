ebf8e6e9b2a2c1e1fce3e5970bc14239a374f1c2
ShengkunZhang <zskkun131@126.com>
2016-01-12 18:36:43 +0800

    VC添加注释

--- GithubLaboratoryA/ViewController.m
+++ GithubLaboratoryA/ViewController.m
@@ -16,7 +16,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    /*
+        初始版
+     */
 }
 
 - (void)didReceiveMemoryWarning {
