3f97e1f93452ae46fdbb40cd6a2fb2c27a906bf3
zhang shengkun <zskkun131@126.com>
2019-05-17 18:19:51 +0800

    测试冲突

--- GithubLaboratoryA/ViewController.m
+++ GithubLaboratoryA/ViewController.m
@@ -16,17 +16,13 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    /*
-        初始版
-     */
-    NSLog(@"1234546");
-    NSLog(@"987654321");
     
     // 测试版本回退问题
     
     // viewController 文件冲突测试
     
     // 第二次文件冲突测试
+    NSLog(@"test");
 }
 
 - (void)didReceiveMemoryWarning {
