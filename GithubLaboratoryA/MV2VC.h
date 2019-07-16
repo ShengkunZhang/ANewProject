879f266a093c53d807ee8935f184cada4594edfc
zhangshengkun <zhangshengkun@im30.net>
2019-07-16 11:36:26 +0800

    合并冲突

---
+++ GithubLaboratoryA/MV2VC.h
@@ -0,0 +1,17 @@
+//
+//  MV2VC.h
+//  GithubLaboratoryA
+//
+//  Created by zsk on 2019/7/16.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MV2VC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GithubLaboratoryA/MV2VC.m
@@ -0,0 +1,32 @@
+//
+//  MV2VC.m
+//  GithubLaboratoryA
+//
+//  Created by zsk on 2019/7/16.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import "MV2VC.h"
+
+@interface MV2VC ()
+
+@end
+
+@implementation MV2VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"这个事MV2分支的文件");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
