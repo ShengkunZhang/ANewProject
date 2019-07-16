bd9aa3a857a5a1d38d9a847dd77c855399a72398
zhangshengkun <zhangshengkun@im30.net>
2019-07-16 11:31:54 +0800

    添加MV1VC文件

---
+++ GithubLaboratoryA/MV1VC.h
@@ -0,0 +1,17 @@
+//
+//  MV1VC.h
+//  GithubLaboratoryA
+//
+//  Created by zsk on 2019/7/16.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MV1VC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GithubLaboratoryA/MV1VC.m
@@ -0,0 +1,32 @@
+//
+//  MV1VC.m
+//  GithubLaboratoryA
+//
+//  Created by zsk on 2019/7/16.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import "MV1VC.h"
+
+@interface MV1VC ()
+
+@end
+
+@implementation MV1VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"这个事mv1分支的文件");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
