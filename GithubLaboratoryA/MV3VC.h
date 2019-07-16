c9605500857eff8da3d92b1e7c9b77f6a355a2de
zhangshengkun <zhangshengkun@im30.net>
2019-07-16 11:38:02 +0800

    添加MV3VC文件

---
+++ GithubLaboratoryA/MV3VC.h
@@ -0,0 +1,17 @@
+//
+//  MV3VC.h
+//  GithubLaboratoryA
+//
+//  Created by zsk on 2019/7/16.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MV3VC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ GithubLaboratoryA/MV3VC.m
@@ -0,0 +1,32 @@
+//
+//  MV3VC.m
+//  GithubLaboratoryA
+//
+//  Created by zsk on 2019/7/16.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+#import "MV3VC.h"
+
+@interface MV3VC ()
+
+@end
+
+@implementation MV3VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    NSLog(@"这个事MV3分支的文件");
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
